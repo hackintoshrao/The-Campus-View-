4fd7d63a80142cdeaa64338186b1bb4441ace2f1
Karthic Rao <kartronics85@gmail.com>
2013-03-02 07:32:50 +0530

    connect middleware server

--- NODE-PIC-VIDEO-WEB-SERVER.js
+++ NODE-PIC-VIDEO-WEB-SERVER.js
@@ -1,43 +1,8 @@
-var http=require('http'),
-	url = require('url'),
-	mime = require('mime'),//npm install mime
-	fs = require('fs'),
-	search_path='/home/karthic/mydata/codes';
-http.createServer(function(req,res) {
-	 pathname = search_path + req.url;
-	 console.log(req.url);
-	 console.log(pathname);
+var connect = require('connect'),
+http = require('http'),
+_dirname = '/home/karthic/Videos';
+http.createServer(connect()
+.use(connect.logger())
+.use(connect.static(_dirname ), {redirect: true})
+).listen(8124);
 
-	fs.stat(pathname , function(err,stats){
-	  if(err) { 
-		res.writeHead(404);
-		res.write('Bad Request , Request cannot be accepted');
-		res.end()
-		}
-	else if(stats.isFile()) {
-		//content-type
-		var type = mime.lookup(pathname);
-		console.log(type);
-		res.setHeader('Content-Type',type);
-
-		// 200 status - found , no errors 
-		res.statusCode = 200;
-	
-		//create a readable stream and pipe to the http response 
-		var file = fs.createReadStream(pathname);
-		file.on("open",function() { 
-			file.pipe(res);
-		});
-		file.on("error",function(err) {
-			cosole.log(err);
-		});
-	     }
-	else {
-		res.writeHead(403);
-		res.write('Cannot Access Directory');
-		res.end();
-		}
-	});
-}).listen(8124);
-console.log('Server running at 8124');
-		
