0dc9bfe5089b36adae11ed9c9867275fbf9a517b
Karthic Rao <kartronics85@gmail.com>
2013-03-02 16:50:12 +0530

    Deletion of old server

--- NODE-PIC-VIDEO-WEB-SERVER.js
+++
@@ -1,8 +0,0 @@
-var connect = require('connect'),
-http = require('http'),
-_dirname = '/home/karthic/Videos';
-http.createServer(connect()
-.use(connect.logger())
-.use(connect.static(_dirname ), {redirect: true})
-).listen(8124);
-
